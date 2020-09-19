466237a95130627cdca1679c10d9c70192fea40d
lacville76 <lacville76@gmail.com>
2020-09-19 04:18:23 -0700

    update for list downlading

--- main.js
+++ main.js
@@ -4,17 +4,17 @@ const mongodb = require('mongodb');
 async function main() {
     const browser = await puppeteer.launch({
         // headless: false, // launch headful mode
-        //  slowMo: 250,
-        //  devtools: true // slow down puppeteer script so that it's easier to follow visually
+        // slowMo: 250,
+        // devtools: true // slow down puppeteer script so that it's easier to follow visually
     });
     const page = await browser.newPage();
-    
+    page.setDefaultNavigationTimeout(0);
     page.on('console', msg => {
         console.log(msg.text());
     });
 
     await page.goto('https://www.bricklink.com/browse.asp');
-    console.log("1/3 loading countries ..")
+    console.log("1/3 loading countries ..");
     let countries = await page.evaluate(() => {
         // getting first 4 containers - countries grouped by 4 regions
         let regions = Array.from(document.getElementsByClassName('store-list')).slice(0,4);
@@ -61,7 +61,7 @@ async function main() {
 function getItems(items,shop,document){
     Array.from(document.getElementsByTagName("article")).forEach(article => {
         const description = article.getElementsByClassName("description").item(0);
-        const name = description.getElementsByTagName("strong").item(0).innerText;
+        const name = description.getElementsByTagName("p").item(0).innerText;
         const caption = description.getElementsByClassName("caption").item(0).innerText;
         const condition = article.getElementsByClassName("condition").item(0).innerText;
         let path = "";
@@ -70,14 +70,21 @@ function getItems(items,shop,document){
         });
         const buy = article.getElementsByClassName("buy").item(0);
         const quantity = Array.from(buy.getElementsByTagName("strong"))[0].innerText;
-        const price = Array.from(buy.getElementsByTagName("p"))[1].innerText.split(" ")[1].replace(")","");
+        let price = buy.getElementsByTagName("div").item(0).getElementsByTagName("p").item(0);
+        if(price) {
+            price = price.innerText;
+        }
+        else {
+            price = buy.getElementsByTagName("div").item(0).getElementsByTagName("strong").item(0).innerText;
+        }
+        price = price.replace(/[~$()EURUS]/g,"");
         items.push({
             "name":name,
             "condition":condition.replace("\n"," "),
             "quantity":quantity,
             "caption":caption,
-            "price":Math.floor(parseFloat(price.replace(",","")) * 100) / 100,
-            "path":path,
+            "price":""+Math.floor(parseFloat(price.replace(",","")) * 100) / 100,
+            "path":path, 
             "date": new Date().toUTCString(),
             "country": shop.country,
             "shop": shop.name
@@ -91,6 +98,9 @@ async function getShops(shops,page,mongoDb) {
         const start = new Date();
         await page.goto(shop.link+'#/shop?o={"pgSize":100,"showHomeItems":0}',  { waitUntil: "networkidle0" });
         const pageCount = await page.evaluate(() => {
+            if(!document.getElementsByClassName("pagination").item(0)) {
+                return 0;
+            }
             const paginationNavbar =  document.getElementsByClassName("pagination").item(0).children;
             const pageCount = paginationNavbar[paginationNavbar.length - 2].getElementsByTagName("a").item(0).innerText;
             return pageCount;
@@ -110,6 +120,7 @@ async function getShops(shops,page,mongoDb) {
                 },shop,items));
             }
         }
+        if(items && items.length > 0)
         await mongoDb.collection("brick_data").insertMany(items, function(err, res) {
             if (err) throw err;
             console.log("-------------");
